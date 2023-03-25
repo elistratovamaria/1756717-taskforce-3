4bed3dc65edcf1a3ecdf033e98e8c52db603f99d
elistratovamaria <Elistratova.Mari.22@yandex.ru>
2023-03-25 14:27:41 +0300

    Добавляет метод comparePassword

--- project/apps/users/src/app/platform-user/platform-user.entity.ts
+++ project/apps/users/src/app/platform-user/platform-user.entity.ts
@@ -1,5 +1,5 @@
 import { User, UserRole, City } from '@project/shared/shared-types';
-import { genSalt, hash } from 'bcrypt';
+import { compare, genSalt, hash } from 'bcrypt';
 import { SALT_ROUNDS } from './platform-user.constant';
 
 export class PlatformUserEntity implements User {
@@ -35,5 +35,9 @@ export class PlatformUserEntity implements User {
     const salt = await genSalt(SALT_ROUNDS);
     this.passwordHash = await hash(password, salt);
     return this;
+  }
+
+  public async comparePassword(password: string): Promise<boolean> {
+    return compare(password, this.passwordHash);
   }
 }
