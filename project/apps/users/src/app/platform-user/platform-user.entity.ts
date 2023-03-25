c109e0f8056be73e87a664756664cc630937f4ad
elistratovamaria <Elistratova.Mari.22@yandex.ru>
2023-03-25 12:56:48 +0300

    Добавляет сущность PlatformUserEntity

---
+++ project/apps/users/src/app/platform-user/platform-user.entity.ts
@@ -0,0 +1,31 @@
+import { User, UserRole, City } from '@project/shared/shared-types';
+
+export class PlatformUserEntity implements User {
+  public _id: string;
+  public name: string;
+  public email: string;
+  public city: City;
+  public passwordHash: string;
+  public role: UserRole;
+  public avatar?: string;
+  public dateBirth: Date;
+
+  constructor(platformUser: User) {
+    this.fillEntity(platformUser);
+  }
+
+  public toObject() {
+    return {...this};
+  }
+
+  public fillEntity(platformUser: User) {
+    this._id = platformUser._id;
+    this.name = platformUser.name;
+    this.email = platformUser.email;
+    this.city = platformUser.city;
+    this.passwordHash = platformUser.passwordHash;
+    this.role = platformUser.role;
+    this.avatar = platformUser.avatar;
+    this.dateBirth = platformUser.dateBirth;
+  }
+}
