437d24c22dec994e4cc200d356804599df2801de
elistratovamaria <Elistratova.Mari.22@yandex.ru>
2023-03-25 19:14:21 +0300

    Добавляет CreateUserDto и LoginUserDto

---
+++ project/apps/users/src/app/authentication/dto/create-user.dto.ts
@@ -0,0 +1,11 @@
+import { UserRole, City } from '@project/shared/shared-types';
+
+export class CreateUserDto {
+  public name: string;
+  public email: string;
+  public city: City;
+  public password: string;
+  public role: UserRole;
+  public avatar?: string;
+  public dateBirth: Date;
+}
