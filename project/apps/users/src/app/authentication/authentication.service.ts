30e15081e3aa4efd5a1e289b50c101491ee00126
elistratovamaria <Elistratova.Mari.22@yandex.ru>
2023-03-25 19:05:35 +0300

    Внедряет репозиторий в качестве зависимости

--- project/apps/users/src/app/authentication/authentication.service.ts
+++ project/apps/users/src/app/authentication/authentication.service.ts
@@ -1,4 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import { PlatformUserMemoryRepository } from '../platform-user/platform-user-memory.repository';
 
 @Injectable()
-export class AuthenticationService {}
+export class AuthenticationService {
+  constructor(
+    private readonly platformUserRepository: PlatformUserMemoryRepository
+  ) {}
+}
