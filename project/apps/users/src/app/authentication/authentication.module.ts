c9a29942a9887b62f6378362c0c5c5201bb90d35
elistratovamaria <Elistratova.Mari.22@yandex.ru>
2023-03-25 19:03:39 +0300

    Добавляет импорт модуля PlatformUserModule

--- project/apps/users/src/app/authentication/authentication.module.ts
+++ project/apps/users/src/app/authentication/authentication.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthenticationController } from './authentication.controller';
 import { AuthenticationService } from './authentication.service';
+import { PlatformUserModule } from '../platform-user/platform-user.module';
 
 @Module({
+  imports: [PlatformUserModule],
   controllers: [AuthenticationController],
   providers: [AuthenticationService],
 })
