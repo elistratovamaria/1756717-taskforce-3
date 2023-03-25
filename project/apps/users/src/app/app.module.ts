a5a1aea6f501e20937f23ea77bb405ec245a43c5
elistratovamaria <Elistratova.Mari.22@yandex.ru>
2023-03-25 12:32:55 +0300

    Добавляет модули AuthenticationModule и PlatformUserModule

--- project/apps/users/src/app/app.module.ts
+++ project/apps/users/src/app/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { PlatformUserModule } from './platform-user/platform-user.module';
+import { AuthenticationModule } from './authentication/authentication.module';
 
 @Module({
-  imports: [],
+  imports: [PlatformUserModule, AuthenticationModule],
   controllers: [],
   providers: [],
 })
