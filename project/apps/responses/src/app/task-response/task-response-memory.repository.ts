824c40280eb9d085d9195e44a6e229d527e9a646
elistratovamaria <Elistratova.Mari.22@yandex.ru>
2023-03-26 20:23:33 +0300

    Добавляет TaskResponseMemoryRepository

---
+++ project/apps/responses/src/app/task-response/task-response-memory.repository.ts
@@ -0,0 +1,32 @@
+import { CRUDRepository } from '@project/util/util-types';
+import { TaskResponseEntity } from './task-response.entity';
+import { Response } from '@project/shared/shared-types';
+import crypto from 'node:crypto';
+
+export class TaskResponseMemoryRepository implements CRUDRepository<TaskResponseEntity, string, Response> {
+  private repository: {[key: string]: Response} = {};
+
+  public async create(item: TaskResponseEntity): Promise<Response> {
+    const entry = { ...item.toObject(), _id: crypto.randomUUID()};
+    this.repository[entry._id] = entry;
+
+    return {...entry};
+  }
+
+  public async findById(id: string): Promise<Response> {
+    if (this.repository[id]) {
+      return {...this.repository[id]};
+    }
+
+    return null;
+  }
+
+  public async destroy(id: string): Promise<void> {
+    delete this.repository[id];
+  }
+
+  public async update(id: string, item: TaskResponseEntity): Promise<Response> {
+    this.repository[id] = {...item.toObject(), _id: id};
+    return this.findById(id);
+  }
+}
