5284f56f1757a24b967cb981f6038f924b6ed72f
elistratovamaria <Elistratova.Mari.22@yandex.ru>
2023-03-26 18:50:37 +0300

    Добавляет заготовки для TaskCommentController и TaskCommentService

--- project/apps/comments/src/app/task-comment/task-comment.module.ts
+++ project/apps/comments/src/app/task-comment/task-comment.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { TaskCommentController } from './task-comment.controller';
+import { TaskCommentService } from './task-comment.service';
 
-@Module({})
+@Module({
+  controllers: [TaskCommentController],
+  providers: [TaskCommentService],
+})
 export class TaskCommentModule {}
