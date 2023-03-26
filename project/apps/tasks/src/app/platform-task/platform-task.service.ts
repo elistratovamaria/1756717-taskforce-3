2b3764792d111b2e9e303bc53e35ff0ace7ef9c4
elistratovamaria <Elistratova.Mari.22@yandex.ru>
2023-03-26 16:48:44 +0300

    Добавляет метод createTask

--- project/apps/tasks/src/app/platform-task/platform-task.service.ts
+++ project/apps/tasks/src/app/platform-task/platform-task.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { PlatformTaskMemoryRepository } from './platform-task-memory.repository';
+import { PlatformTaskEntity } from './platform-task.entity';
 
 @Injectable()
-export class PlatformTaskService {}
+export class PlatformTaskService {
+  constructor(
+    private readonly platformTaskRepository: PlatformTaskMemoryRepository
+  ) {}
+
+  public async createTask(dto: CreateTaskDto) {
+    const platformTask = dto;
+
+    const taskEntity = await new PlatformTaskEntity(platformTask);
+
+    return this.platformTaskRepository
+      .create(taskEntity);
+  }
+
+
+}
