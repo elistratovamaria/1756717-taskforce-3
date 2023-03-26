24d9482ba78d0b008b45e348f9f577afcf1c0830
elistratovamaria <Elistratova.Mari.22@yandex.ru>
2023-03-26 19:51:49 +0300

    Добавляет настройки для swagger (comments)

--- project/apps/comments/src/app/task-comment/dto/create-comment.dto.ts
+++ project/apps/comments/src/app/task-comment/dto/create-comment.dto.ts
@@ -1,4 +1,15 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CreateCommentDto {
+  @ApiProperty({
+    description: 'Comment message',
+    example: 'Легкое задание'
+  })
   public message: string;
+
+  @ApiProperty({
+    description: 'Task ID',
+    example: '234'
+  })
   public taskId: string;
 }
--- project/apps/comments/src/app/task-comment/task-comment.controller.ts
+++ project/apps/comments/src/app/task-comment/task-comment.controller.ts
@@ -1,21 +1,36 @@
-import { Controller, Body, Post, Delete, Param } from '@nestjs/common';
+import { Controller, Body, Post, Delete, Param, HttpStatus } from '@nestjs/common';
 import { TaskCommentService } from './task-comment.service';
 import { CreateCommentDto } from './dto/create-comment.dto';
 import { fillObject } from '@project/util/util-core';
 import { TaskCommentRdo } from './rdo/task-comment.rdo';
+import { ApiTags, ApiResponse } from '@nestjs/swagger';
 
+@ApiTags('comments')
 @Controller('comments')
 export class TaskCommentController {
   constructor(
     private readonly commentService: TaskCommentService
   ) {}
 
+  @ApiResponse({
+    type: TaskCommentRdo,
+    status: HttpStatus.CREATED,
+    description: 'The comment has been successfully created'
+  })
   @Post()
   public async create(@Body() dto: CreateCommentDto) {
     const newComment = await this.commentService.createComment(dto);
     return fillObject(TaskCommentRdo, newComment);
   }
 
+  @ApiResponse({
+    status: HttpStatus.NO_CONTENT,
+    description: 'The comment has been successfully deleted'
+  })
+  @ApiResponse({
+    status: HttpStatus.FORBIDDEN,
+    description: 'The user does not have enough rights to delete the comment'
+  })
   @Delete(':id')
   public async delete(@Param('id') id: string) {
     this.commentService.deleteComment(id);
