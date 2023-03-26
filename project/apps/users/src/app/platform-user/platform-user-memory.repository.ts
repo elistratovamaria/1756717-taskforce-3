8245848ad006d047ac613e00e76b2ef47ead82e3
elistratovamaria <Elistratova.Mari.22@yandex.ru>
2023-03-26 13:31:55 +0300

    Создает приложение tasks

--- project/apps/users/src/app/platform-user/platform-user-memory.repository.ts
+++ project/apps/users/src/app/platform-user/platform-user-memory.repository.ts
@@ -2,7 +2,7 @@ import { CRUDRepository } from '@project/util/util-types';
 import { PlatformUserEntity } from './platform-user.entity';
 import { User } from '@project/shared/shared-types';
 import { Injectable } from '@nestjs/common';
-import crypto from 'crypto';
+import crypto from 'node:crypto';
 
 @Injectable()
 export class PlatformUserMemoryRepository implements CRUDRepository<PlatformUserEntity, string, User> {
