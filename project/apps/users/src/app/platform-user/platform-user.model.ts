3dd3ec94fc1152ea012844fe088a3eea06d73487
elistratovamaria <Elistratova.Mari.22@yandex.ru>
2023-04-05 23:59:06 +0300

    Добавляет модель PlatformUserModel

---
+++ project/apps/users/src/app/platform-user/platform-user.model.ts
@@ -0,0 +1,50 @@
+import { Document } from 'mongoose';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { User, UserRole, City } from '@project/shared/shared-types';
+
+@Schema({
+  collection: 'users',
+  timestamps: true,
+})
+export class PlatformUserModel extends Document implements User {
+  @Prop({
+    required: true,
+  })
+  public name: string;
+
+  @Prop({
+    required: true,
+  })
+  email: string;
+
+  @Prop({
+    required: true,
+    type: String,
+    enum: City
+  })
+  city: City;
+
+  @Prop({
+    required: true,
+  })
+  passwordHash: string;
+
+  @Prop({
+    required: true,
+    type: String,
+    enum: UserRole,
+  })
+  role: UserRole;
+
+  @Prop({
+    default: '',
+  })
+  avatar?: string;
+
+  @Prop({
+    required: true,
+  })
+  dateBirth: Date;
+}
+
+export const PlatformUserSchema = SchemaFactory.createForClass(PlatformUserModel);
