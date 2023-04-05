d7e810c494c808dea70c52501f5551d6a1951cc8
elistratovamaria <Elistratova.Mari.22@yandex.ru>
2023-04-05 20:13:16 +0300

    Подключает библиотеку ConfigUsersModule в users/AppModule

--- project/apps/users/src/app/app.module.ts
+++ project/apps/users/src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PlatformUserModule } from './platform-user/platform-user.module';
 import { AuthenticationModule } from './authentication/authentication.module';
+import { ConfigUsersModule } from '@project/config/config-users';
 
 @Module({
-  imports: [PlatformUserModule, AuthenticationModule],
+  imports: [PlatformUserModule, AuthenticationModule, ConfigUsersModule],
   controllers: [],
   providers: [],
 })
--- project/libs/config/config-users/src/lib/config-users.module.ts
+++ project/libs/config/config-users/src/lib/config-users.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import appConfig from './app.config';
-import dbConfig from '.db.config';
+import dbConfig from './db.config';
 
-const ENV_USERS_FILE_PATH = 'apps/users/.users.env';
+const ENV_USERS_FILE_PATH = 'apps/users/users.dev.env';
 
 @Module({
   imports: [
@@ -15,4 +15,4 @@ const ENV_USERS_FILE_PATH = 'apps/users/.users.env';
     }),
   ]
 })
-export class ConfigConfigUsersModule {}
+export class ConfigUsersModule {}
