a195a2e77b58124c4c1d81d16f63558ab8830bde
elistratovamaria <Elistratova.Mari.22@yandex.ru>
2023-04-10 23:04:26 +0300

    Добавляет сидирование базы данных

---
+++ project/libs/models/models-tasks/src/prisma/seed.ts
@@ -0,0 +1,87 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+async function fillDb() {
+  await prisma.category.upsert({
+    where: { categoryId: 1 },
+    update: {},
+    create: {
+      title: 'Программирование',
+      tasks: {
+        create: [
+          {
+            title: 'Сверстать лендинг',
+            description: 'Сверстать лендинг для продажи автомасел',
+            userId: '13',
+            price: 1000,
+            city: 'Москва',
+            response: {
+              create: {
+                message: 'Выполнено отлично!',
+                userId: '13',
+                estimation: 5
+              }
+            },
+            comments: {
+              create: [
+                {
+                  message: 'Уточните, пожалуйста, срок исполнения',
+                  userId: '22'
+                }
+              ]
+            }
+          },
+        ]
+      },
+    }
+  });
+  await prisma.category.upsert({
+    where: { categoryId: 2 },
+    update: {},
+    create: {
+      title: 'Ремонт',
+      tasks: {
+        create: [
+          {
+            title: 'Починить телефон',
+            description: 'Что-то случилось с экраном телефона',
+            userId: '14',
+            city: 'Владивосток',
+            response: {
+              create: {
+                message: 'Очень быстро починили телефон',
+                userId: '14',
+                estimation: 5
+              }
+            },
+            comments: {
+              create: [
+                {
+                  message: 'Уточните адрес',
+                  userId: '24'
+                },
+                {
+                  message: 'Уточните марку телефона',
+                  userId: '100'
+                }
+              ]
+            },
+          },
+        ]
+      }
+    }
+  });
+  console.info('🤘️ Database was filled')
+}
+
+fillDb()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (err) => {
+    console.error(err);
+    await prisma.$disconnect()
+
+    process.exit(1);
+  })
