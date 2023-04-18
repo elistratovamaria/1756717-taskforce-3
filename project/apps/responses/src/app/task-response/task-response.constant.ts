99b8fdeb44effeb02d6d71ff8517bb2006f57917
elistratovamaria <Elistratova.Mari.22@yandex.ru>
2023-04-18 18:39:34 +0300

    Добавляет базовую валидацию для DTO сервиса responses

---
+++ project/apps/responses/src/app/task-response/task-response.constant.ts
@@ -0,0 +1,4 @@
+export const enum TaskResponseValidationMessage {
+  MessageNotValid = 'Message should be from 50 to 500 symbols',
+  EstimationNotValid = 'Estimation should be a number from 1 to 5'
+}
