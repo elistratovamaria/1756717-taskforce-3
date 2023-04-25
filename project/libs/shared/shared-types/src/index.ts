765112af90385282819adc2acd4c1e397f028cd1
elistratovamaria <Elistratova.Mari.22@yandex.ru>
2023-04-25 19:24:36 +0300

    Добавляет перечисление RabbitRouting

--- project/libs/shared/shared-types/src/index.ts
+++ project/libs/shared/shared-types/src/index.ts
@@ -9,3 +9,4 @@ export * from './lib/category.interface';
 export * from './lib/file.interface';
 export * from './lib/token-payload.interface';
 export * from './lib/subscriber.interface';
+export * from './lib/rabbit-routing.enum';
