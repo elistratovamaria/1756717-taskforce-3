8cb442edecd4b967cd2766c7ae63d3777605a203
elistratovamaria <Elistratova.Mari.22@yandex.ru>
2023-05-02 19:27:30 +0300

    Добавляет валидацию возраста пользователя

--- project/apps/users/src/app/authentication/authentication.constant.ts
+++ project/apps/users/src/app/authentication/authentication.constant.ts
@@ -14,3 +14,12 @@ export const enum AuthUserValidationMessage {
   DateBirthNotValid = 'The date of birth is not valid',
   DateBirthMinor = 'User should be older than 18',
 }
+
+export const enum UserSetting {
+  NameMinLength = 3,
+  NameMaxLength = 50,
+  PasswordMinLength = 6,
+  PasswordMaxLength = 12,
+  DescriptionMaxLength = 300,
+  MinimumAge = 18,
+}
