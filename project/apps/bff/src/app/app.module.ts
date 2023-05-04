9274df59026f608a3a0a6f37f2b4dca293a6d7f3
elistratovamaria <Elistratova.Mari.22@yandex.ru>
2023-05-04 14:37:45 +0300

    Добавляет заготовки модулей для API

--- project/apps/bff/src/app/app.module.ts
+++ project/apps/bff/src/app/app.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
+import { UsersController } from './users.controller';
+import { TasksController } from './tasks.controller';
 
 @Module({
   imports: [],
   controllers: [],
-  providers: [],
+  providers: [
+    UsersController,
+    TasksController
+  ],
 })
 export class AppModule {}
